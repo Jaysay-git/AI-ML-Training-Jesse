c0552fd86da2b44d1648f3cfc093d2ec52c09bb9
haven-ship-it <havendesignops@gmail.com>
2026-08-24 10:49:27 +0100

    Add Week 3 PostgreSQL CI workflow

--- Week 3/database.py
+++ Week 3/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
-DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5433/hospital_patients"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg://postgres:postgres@localhost:5433/hospital_patients"
+)
 
 engine = create_engine(DATABASE_URL)
 
